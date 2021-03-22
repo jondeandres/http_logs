939a72b5523368678301614f2f12409f0803663c
Jon de Andres <jon@rollbar.com>
2021-03-22 18:41:20 +0100

    Move tasks to pipeline.py

--- http_log/main.py
+++ http_log/main.py
@@ -1,16 +1,11 @@
 import argparse
 import asyncio
-import aiofiles
 import logging
 import signal
-import types
-import time
+import typing
 
 
-from http_log.alert import Alert
-from http_log.sliding_time_window import SlidingTimeWindow
-from http_log import parsing
-from http_log.stats import Stats
+from http_log import pipeline
 
 
 log = logging.getLogger(__name__)
@@ -18,11 +13,9 @@ log = logging.getLogger(__name__)
 
 _DEFAULT_THRESHOLD = 10
 _DEFAULT_LOG_FILE_PATH = '/tmp/access.log'
-_ALERT_WINDOW_SIZE = 120
-_STATS_WINDOW_SIZE = 10
 
 
-def bootstrap():
+def bootstrap() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument('--threshold', default=_DEFAULT_THRESHOLD, type=int)
     parser.add_argument('--log-file-path', default=_DEFAULT_LOG_FILE_PATH)
@@ -32,20 +25,10 @@ def bootstrap():
     asyncio.run(main(args))
 
 
-async def main(args: argparse.Namespace):
+async def main(args: argparse.Namespace) -> None:
     logging.basicConfig(level='INFO')
 
-    alert_window = SlidingTimeWindow(_ALERT_WINDOW_SIZE, 0)
-    stats_window = SlidingTimeWindow(_STATS_WINDOW_SIZE, Stats())
-    alert = Alert(alert_window, args.threshold)
-    queue = asyncio.Queue()
-
-    task = asyncio.gather(
-        read(queue, args.log_file_path),
-        alerts(alert),
-        stats(stats_window),
-        process(queue, alert_window, stats_window)
-    )
+    task = pipeline.build_pipeline_task(args.log_file_path, args.threshold)
 
     handler = signal_handler_factory(task)
 
@@ -65,49 +48,3 @@ def signal_handler_factory(task):
         task.cancel()
 
     return signal_handler
-
-
-async def read(queue, log_file_path):
-    async with aiofiles.open(log_file_path, 'r') as f:
-        await f.seek(0, 2)
-
-        while True:
-            lines = await f.readlines()
-
-            if not lines:
-                await asyncio.sleep(0.500)
-
-                continue
-
-            for line in lines:
-                await queue.put(parsing.parse_line(line))
-
-
-async def process(queue, alert_window, stats_window):
-    while True:
-        entry = await queue.get()
-
-        alert_window.add(entry.timestamp, 1)
-        stats_window.add(
-            entry.timestamp,
-            Stats(
-                total=1,
-                codes={entry.status_code: 1},
-                sections={entry.section: 1}
-            )
-        )
-
-        queue.task_done()
-
-
-async def alerts(alert):
-    while True:
-        await asyncio.sleep(1)
-        alert.run()
-
-
-async def stats(window):
-    while True:
-        await asyncio.sleep(10)
-        window.expire(time.time())
-        print(sorted(window.value.codes.items(), key=lambda x: -x[1]))
---
+++ http_log/pipeline.py
@@ -0,0 +1,74 @@
+import aiofiles
+import asyncio
+import time
+import typing
+import collections.abc
+
+from http_log import parsing
+from http_log.alert import Alert
+from http_log.sliding_time_window import SlidingTimeWindow
+from http_log.stats import Stats
+
+
+_ALERT_WINDOW_SIZE = 120
+_STATS_WINDOW_SIZE = 10
+
+
+def build_pipeline_task(log_file_path: str, alert_threshold: int) -> collections.abc.Awaitable:
+    alert_window = SlidingTimeWindow(_ALERT_WINDOW_SIZE, 0)
+    stats_window = SlidingTimeWindow(_STATS_WINDOW_SIZE, Stats())
+    alert = Alert(alert_window, alert_threshold)
+    queue = asyncio.Queue()
+
+    return asyncio.gather(
+        _read(queue, log_file_path),
+        _alerts(alert),
+        _stats(stats_window),
+        _process(queue, alert_window, stats_window)
+    )
+
+
+async def _read(queue: asyncio.Queue, log_file_path: str):
+    async with aiofiles.open(log_file_path, 'r') as f:
+        await f.seek(0, 2)
+
+        while True:
+            lines = await f.readlines()
+
+            if not lines:
+                await asyncio.sleep(0.500)
+
+                continue
+
+            for line in lines:
+                await queue.put(parsing.parse_line(line))
+
+
+async def _process(queue: asyncio.Queue, alert_window: SlidingTimeWindow, stats_window: SlidingTimeWindow) -> None:
+    while True:
+        entry = await queue.get()
+
+        alert_window.add(entry.timestamp, 1)
+        stats_window.add(
+            entry.timestamp,
+            Stats(
+                total=1,
+                codes={entry.status_code: 1},
+                sections={entry.section: 1}
+            )
+        )
+
+        queue.task_done()
+
+
+async def _alerts(alert: Alert) -> None:
+    while True:
+        await asyncio.sleep(1)
+        alert.run()
+
+
+async def _stats(window: SlidingTimeWindow) -> None:
+    while True:
+        await asyncio.sleep(10)
+        window.expire(time.time())
+        print(sorted(window.value.codes.items(), key=lambda x: -x[1]))
